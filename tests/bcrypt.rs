569fc839f15d93cc0aadf38b0f1660ce14c53478
pas2rust <145881352+pas2rust@users.noreply.github.com>
2024-01-21 14:32:58 -0300

    Update bcrypt.rs

--- tests/bcrypt.rs
+++ tests/bcrypt.rs
@@ -3,13 +3,13 @@ use wrapper_bcrypt::wrapper::{BcryptTrait, WrapperBcrypt};
 fn bcrypt_gen_hash_and_verify_password() {
     let mut wrapper = WrapperBcrypt::new("12345".to_string(), 13, None);
     let hash = wrapper.encode().expect("hash error");
-    wrapper.set_hash(Some(hash));
+    wrapper.set_hash(hash);
     let verify = wrapper.verify();
     match verify {
         Ok(ok) => assert!(ok),
         Err(err) => {
             wrapper.print_err();
             assert!(false, "{}", err)
-        },
+        }
     };
 }
